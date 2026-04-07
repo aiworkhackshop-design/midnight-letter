17284446659696ac7f64c5d3f8b047c4b175b39e
aiworkhackshop-design <ai.workhack.shop@gmail.com>
2026-04-07 15:44:37 +0900

    Add button component

---
+++ components/ui/button.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import * as React from "react"
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button({ className = "", ...props }: ButtonProps) {
+  return <button className={className} {...props} />
+}
